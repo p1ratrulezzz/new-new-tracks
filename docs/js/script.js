77b276852af611757038c8bc3665e795fd5629b7
p1ratrulezzz <git@p1ratrulezzz.me>
2023-01-10 10:34:22 +0300

    Fix script

--- docs/js/script.js
+++ docs/js/script.js
@@ -10,21 +10,43 @@
         return box.top < window.innerHeight && box.bottom >= 0;
     }
 
+    let scrollData = {
+        break: false,
+        in_progress: false
+    };
+
     function lazyLoaderScroll() {
+        if (scrollData.in_progress) {
+            scrollData.break = true;
+        }
+
         let $lazyElements = $('.lazy-spotify-loader');
         $lazyElements.each(function(index, element) {
-            if (isInView(element)) {
-                let $element = $(element);
-                $element.removeClass('lazy-spotify-loader');
-                let embed = $element.attr('data-spotify-embed');
-                embed = atob(embed);
-                $element.html(embed);
-                $element.removeAttr('data-spotify-embed');
-            }
-            else {
+            if (scrollData.break) {
+                scrollData.break = false;
+                scrollData.in_progress = false;
                 return;
             }
+
+            scrollData.in_progress = true;
+
+            let $element = $(element);
+            if (isInView(element.parentElement)) {
+                if ($element.attr('data-spotify-embed') != null) {
+                    let embed = $element.attr('data-spotify-embed');
+                    embed = atob(embed);
+                    $element.html(embed);
+                    $element.removeAttr('data-spotify-embed');
+                }
+
+                $element.show();
+            }
+            else {
+                $element.hide();
+            }
         });
+
+        scrollData.in_progress = false
     }
 
     $(window).scroll(lazyLoaderScroll);
